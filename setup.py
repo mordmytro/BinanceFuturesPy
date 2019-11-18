2db4503426f46ebe64adfe0df8b676f3cb7ee27c
morozdima <39806138+morozdima@users.noreply.github.com>
2019-11-18 15:01:51 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='BinanceFuturesPy',
+      version='1.0',
+      description='Python library for Binance Futures and Binance Futures Testnet',
+      author='morozdima',
+      author_email='idima.mor@gmail.com',
+      url='https://github.com/morozdima/BinanceFuturesPy',
+      download_url='https://github.com/morozdima/BinanceFuturesPy/archive/master.zip',
+      license='MIT',
+      install_requires=['websocket-client', 'requests', 'urllib', 'json', 'hmac', 'hashlib', 'pandas'],
+      packages=find_packages()
+      )
