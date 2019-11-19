7cedf802940d9a640e17c152938719304af87f6e
morozdima <39806138+morozdima@users.noreply.github.com>
2019-11-19 11:31:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 
-from setuptools import setup
-from setuptools import find_packages
+import setuptools
 
 
-setup(name='BinanceFuturesPy',
-      version='1.0',
-      description='Python library for Binance Futures and Binance Futures Testnet',
-      author='morozdima',
-      url='https://github.com/morozdima/BinanceFuturesPy',
-      download_url='https://github.com/morozdima/BinanceFuturesPy/archive/master.zip',
-      install_requires=['websocket-client', 'pandas'],
-      packages=find_packages()
-      )
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+setuptools.setup(
+                 name='BinanceFuturesPy',
+                 version='1.1',
+                 scripts=['futurespy'],
+                 author='morozdima',
+                 author_email="dmytro@black-box.ai",
+                 description='Python library for Binance Futures and Binance Futures Testnet',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url='https://github.com/morozdima/BinanceFuturesPy',
+                 download_url='https://github.com/morozdima/BinanceFuturesPy/archive/master.zip',
+                 packages=setuptools.find_packages()
+)
