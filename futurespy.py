d9189c27cae29182b84437947bdc787ecdbb189f
Dima <39806138+morozdima@users.noreply.github.com>
2020-08-26 15:05:38 +0300

    .

--- futurespy.py
+++ futurespy.py
@@ -17,7 +17,7 @@ import threading
 """
     That library have got just a part of Documentation
     If you want to know what does certain function use can find more on
-    
+
     ! ! !
     https://binance-docs.github.io/apidocs/futures/en
     ! ! !
@@ -26,7 +26,11 @@ import threading
 
 class MarketData:
     def __init__(
-        self, testnet: bool = False, symbol: str = "btcusdt", interval: str = "1m"
+        self,
+        api_key=None,
+        testnet: bool = False,
+        symbol: str = "btcusdt",
+        interval: str = "1m",
     ):
 
         """
@@ -54,6 +58,8 @@ class MarketData:
         self.wss_way = "wss://fstream.binance.com/ws/"
         self.interval = interval
         self.symbol = symbol.lower()
+        self.api_key = api_key
+        self.X_MBX_APIKEY = {"X-MBX-APIKEY": self.api_key}
 
     def ping(self):
         return requests.get(f"{self.http_way}ping").json()
@@ -80,14 +86,21 @@ class MarketData:
             f"{self.http_way}trades?symbol={self.symbol}&limit={limit}"
         ).json()
 
-    def historical_trades(self, fromId: int = None, limit: int = 500):
+    def historical_trades(self, limit: int = 500, fromId=None):
         """
         To change limit -> limit = 1000
         (max 1000)
         """
-        return requests.get(
-            f"{self.http_way}historicalTrades?symbol={self.symbol}&fromId={fromId}&limit={limit}"
-        ).json()
+        if fromId:
+            return requests.get(
+                f"{self.http_way}historicalTrades?symbol={self.symbol}&limit={limit}&fromId={fromId}",
+                headers=self.X_MBX_APIKEY,
+            ).json()
+        else:
+            return requests.get(
+                f"{self.http_way}historicalTrades?symbol={self.symbol}&limit={limit}",
+                headers=self.X_MBX_APIKEY,
+            ).json()
 
     def aggregate_trades(
         self,
@@ -106,27 +119,6 @@ class MarketData:
         """
         return requests.get(
             f"{self.http_way}aggTrades?symbol={self.symbol}&fromId={fromId}&startTime={startTime}&endTime={endTime}&limit={limit}"
-        ).json()
-
-    def candles_data(
-        self,
-        interval: str = "1m",
-        startTime: int = None,
-        endTime: int = None,
-        limit: int = 500,
-    ):
-        """
-        To change interval                  ->  interval = '5m'
-        (Valid values: [1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M])
-        
-        To use limit                        ->  limit = 1231
-        (Default 500; max 1500)
-        
-        To use start time and end time      ->  startTime = 1573661424937
-                                            ->  endTime = 1573661428706
-        """
-        return requests.get(
-            f"{self.http_way}klines?symbol={self.symbol}&interval={interval}&startTime={startTime}&endTime={endTime}&limit={limit}"
         ).json()
 
     def mark_price(self):
@@ -170,13 +162,40 @@ class MarketData:
         else:
             return requests.get(f"{self.http_way}ticker/bookTicker").json()
 
-    def load_last_candles(self, days=30):
+    def candles_data(
+        self,
+        interval: str = "1m",
+        startTime: int = None,
+        endTime: int = None,
+        limit: int = 500,
+    ):
+        """
+        To change interval                  ->  interval = '5m'
+        (Valid values: [1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M])
+        
+        To use limit                        ->  limit = 1231
+        (Default 500; max 1500)
+        
+        To use start time and end time      ->  startTime = 1573661424937
+                                            ->  endTime = 1573661428706
+        """
+        return requests.get(
+            f"{self.http_way}klines?symbol={self.symbol}&interval={interval}&startTime={startTime}&endTime={endTime}&limit={limit}"
+        ).json()
+
+    def load_last_candles(self, days=30, on_update=None):
+        """
+        DEPRESIATED
+        Use load_historical_candles instead
+        """
         limit = 1440
 
         one_hour_in_milliseconds = 3600000
         one_day_in_milliseconds = one_hour_in_milliseconds * 24
 
-        startTime = int(round(time.time() * 1000)) - (one_day_in_milliseconds * 30)
+        startTime = int(round(time.time() * 1000)) - (
+            one_day_in_milliseconds * days
+        )  # 30 days = 30, 90 days = 90
 
         data = []
 
@@ -187,11 +206,11 @@ class MarketData:
             startTime += one_day_in_milliseconds
             response = r.json()
 
+            if on_update is not None:
+                on_update(int(100 * (k / days)))
+
             for i in range(len(response)):
                 data.append(response[i])
-            stdout.write(f"\r{k + 1} of {days}")
-            stdout.flush()
-        print("\n")
 
         """
         last_req = requests.get(f"{self.http_way}klines?symbol={self.symbol}&interval={self.interval}&limit=1")
@@ -224,6 +243,54 @@ class MarketData:
 
         return df
 
+    def load_historical_candles(
+        self, count: int = 30000, on_update=None
+    ) -> pd.DataFrame:
+        tm = int(round(time.time() * 1000))
+
+        lim = []
+        if count == 1500:
+            lim.append(count)
+        else:
+            lim.append(count % 1500)
+        while count > 1500:
+            lim.append(1500)
+            count -= 1500
+
+        for i in range(len(lim)):
+            if lim[i] == 0:
+                lim[i] = 1500
+
+        tmp_list = []
+        for i, j in enumerate(reversed(lim)):
+            tmp = self.candles_data(interval=self.interval, limit=j, endTime=tm)
+
+            if on_update is not None:
+                on_update(int(100 * ((i + 1) / len(lim))))
+
+            if type(tmp) != list:
+                print(tmp, i)
+            for candle in reversed(tmp):
+                dd = {
+                    "Date": candle[0],
+                    "Open": float(candle[1]),
+                    "High": float(candle[2]),
+                    "Low": float(candle[3]),
+                    "Close": float(candle[4]),
+                    "Volume": float(candle[5]),
+                }
+                tmp_list.append(dd)
+            if len(tmp) not in lim:
+                print("--- Not enough data ---")
+                return
+            tm = tmp[0][0] - 1
+
+        df = pd.DataFrame(reversed(tmp_list))
+        df["Date"] = pd.to_datetime(df["Date"], unit="ms")
+        df["Date"] = df["Date"].map(lambda x: x.strftime("%Y-%m-%d %H:%M"))
+
+        return df
+
 
 #%%
 
@@ -231,14 +298,12 @@ class MarketData:
 class WebsocketMarket:
     def __init__(
         self,
-        on_message=lambda ws, message: (
-            stdout.write(f"\r{json.loads(message)}"),
-            stdout.flush(),
-        ),
+        on_message=lambda ws, message: print(message),
         on_error=lambda ws, error: print(error),
         on_close=lambda ws: print("### closed ###"),
         testnet: bool = False,
         symbol: str = "btcusdt",
+        interval: str = "1m",
         speed: str = "100ms",
     ):
         """
@@ -256,7 +321,7 @@ class WebsocketMarket:
         else:
             self.wss_way = "wss://fstream.binance.com/ws/"
 
-        self.interval = "1m"
+        self.interval = interval
         self.symbol = symbol.lower()
         self.speed = speed
 
@@ -321,7 +386,12 @@ class WebsocketMarket:
 
 class Client:
     def __init__(
-        self, api_key: str, sec_key: str, testnet: bool = False, symbol: str = "BTCUSDT"
+        self,
+        api_key: str,
+        sec_key: str,
+        testnet: bool = False,
+        symbol: str = "BTCUSDT",
+        recv_window: int = 30000,
     ):
         """
         In any case you must give your API key and API secret to work with Client
@@ -336,6 +406,8 @@ class Client:
         self.symbol = symbol
         self.X_MBX_APIKEY = {"X-MBX-APIKEY": self.api_key}
 
+        self.recvWindow = recv_window
+
         if testnet == True:
             self.http_way = "http://testnet.binancefuture.com/fapi/v1/"
             self.wss_way = "wss://stream.binancefuture.com/ws/"
@@ -343,13 +415,29 @@ class Client:
             self.http_way = "http://fapi.binance.com/fapi/v1/"
             self.wss_way = "wss://fstream.binance.com/ws/"
 
-    def open_socket(self, way, on_message, on_error, on_close):
-        websocket.enableTrace(False)
+    def server_time(self):
+        try:
+            return requests.get(f"{self.http_way}time").json()
+        except Exception as e:
+            self.logger.error(e)
+            return None
 
-        self.ws = websocket.WebSocketApp(
-            way, on_message=on_message, on_error=on_error, on_close=on_close
+    def open_socket(self, way, on_message, on_error, on_close):
+        self.thread = threading.Thread(
+            target=lambda: self._open_socket(way, on_message, on_error, on_close)
         )
-        self.ws.run_forever()
+        self.thread.start()
+
+    def _open_socket(self, way, on_message, on_error, on_close):
+        try:
+            websocket.enableTrace(False)
+
+            self.ws = websocket.WebSocketApp(
+                way, on_message=on_message, on_error=on_error, on_close=on_close
+            )
+            self.ws.run_forever()
+        except Exception as e:
+            self.logger.error(e)
 
     def _get_request(self, req, query):
         r = requests.get(
@@ -414,10 +502,28 @@ class Client:
             else:
                 return r
 
-    @staticmethod
-    def timestamp():
+    def check_keys(self):
+        response = self.balance()
+        return (
+            True
+            if isinstance(response, list)
+            else False
+            if isinstance(response, dict)
+            and "code" in response
+            and response["code"] == -2014
+            else response["msg"]
+            if isinstance(response, dict) and "msg" in response
+            else str(response)
+        )
 
-        return int(time.time() * 1000)
+    def timestamp(self, server: bool = False):
+        if server:
+            server_time = self.server_time()["serverTime"]
+            return (
+                server_time if server_time is not None else self.timestamp(server=False)
+            )
+        else:
+            return int(time.time() * 1000)
 
     def get_sign(self, query):
 
@@ -431,6 +537,7 @@ class Client:
 
     def new_order(
         self,
+        symbol: str,
         side: str,
         orderType: str,
         quantity: float,
@@ -455,7 +562,7 @@ class Client:
         req = "order?"
 
         querystring = {
-            "symbol": self.symbol,
+            "symbol": symbol,
             "side": side,
             "type": orderType,
             "quantity": quantity,
@@ -472,12 +579,29 @@ class Client:
         if workingType is not None:
             querystring["workingType"] = workingType
         querystring["timestamp"] = self.timestamp()
+        querystring["recvWindow"] = self.recvWindow
 
         querystring = urllib.parse.urlencode(querystring)
 
         return self._post_request(req, querystring)
+    
+    def place_multiple_orders(self, orders_list):
+        """
+        POST
+        """
+        req = "batchOrders?"
+        querystring = urllib.parse.urlencode(
+            {
+                "batchOrders": orders_list,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
+            }
+        )
+        querystring = querystring.replace('%27', '%22')
+        
+        return self._post_request(req, querystring)
 
-    def query_order(self, orderId):
+    def query_order(self, symbol: str, orderId, clientID=False):
         """
         GET
         
@@ -485,12 +609,17 @@ class Client:
         """
         req = "order?"
         querystring = urllib.parse.urlencode(
-            {"symbol": self.symbol, "orderId": orderId, "timestamp": self.timestamp()}
+            {
+                "symbol": symbol,
+                "orderId" if not clientID else "origClientOrderId": orderId,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
+            }
         )
 
         return self._get_request(req, querystring)
 
-    def cancel_order(self, orderId):
+    def cancel_order(self, symbol: str, orderId, clientID=False):
         """
         DELETE
         
@@ -498,7 +627,45 @@ class Client:
         """
         req = "order?"
         querystring = urllib.parse.urlencode(
-            {"symbol": self.symbol, "orderId": orderId, "timestamp": self.timestamp()}
+            {
+                "symbol": symbol,
+                "orderId" if not clientID else "origClientOrderId": orderId,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
+            }
+        )
+
+        return self._delete_request(req, querystring)
+
+    def cancel_all_open_orders(self, symbol: str):
+        """
+        DELETE
+        """
+        req = "allOpenOrders?"
+        querystring = urllib.parse.urlencode(
+            {
+                "symbol": symbol,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
+            }
+        )
+
+        return self._delete_request(req, querystring)
+
+    def cancel_multiple_orders(self, symbol: str, orderIdList):
+        """
+        DELETE
+        
+        List of orderIds (max: 10)
+        """
+        req = "batchOrders?"
+        querystring = urllib.parse.urlencode(
+            {
+                "symbol": symbol,
+                "orderIdList": orderIdList,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
+            }
         )
 
         return self._delete_request(req, querystring)
@@ -508,7 +675,9 @@ class Client:
         GET
         """
         req = "openOrders?"
-        querystring = urllib.parse.urlencode({"timestamp": self.timestamp()})
+        querystring = urllib.parse.urlencode(
+            {"recvWindow": self.recvWindow, "timestamp": self.timestamp()}
+        )
 
         return self._get_request(req, querystring)
 
@@ -525,10 +694,11 @@ class Client:
         querystring = urllib.parse.urlencode(
             {
                 "symbol": self.symbol,
-                "timestamp": self.timestamp(),
                 "limit": limit,
                 "startTime": startTime,
                 "endTime": endTime,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
             }
         )
 
@@ -539,7 +709,9 @@ class Client:
         GET
         """
         req = "balance?"
-        querystring = urllib.parse.urlencode({"timestamp": self.timestamp()})
+        querystring = urllib.parse.urlencode(
+            {"recvWindow": self.recvWindow, "timestamp": self.timestamp()}
+        )
 
         return self._get_request(req, querystring)
 
@@ -548,7 +720,9 @@ class Client:
         GET
         """
         req = "account?"
-        querystring = urllib.parse.urlencode({"timestamp": self.timestamp()})
+        querystring = urllib.parse.urlencode(
+            {"recvWindow": self.recvWindow, "timestamp": self.timestamp()}
+        )
 
         return self._get_request(req, querystring)
 
@@ -561,7 +735,12 @@ class Client:
         """
         req = "leverage?"
         querystring = urllib.parse.urlencode(
-            {"symbol": self.symbol, "leverage": leverage, "timestamp": self.timestamp()}
+            {
+                "symbol": self.symbol,
+                "leverage": leverage,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
+            }
         )
 
         return self._post_request(req, querystring)
@@ -569,7 +748,9 @@ class Client:
     def position_info(self):
         """GET"""
         req = "positionRisk?"
-        querystring = urllib.parse.urlencode({"timestamp": self.timestamp()})
+        querystring = urllib.parse.urlencode(
+            {"recvWindow": self.recvWindow, "timestamp": self.timestamp()}
+        )
 
         return self._get_request(req, querystring)
 
@@ -586,10 +767,11 @@ class Client:
         querystring = urllib.parse.urlencode(
             {
                 "symbol": self.symbol,
-                "timestamp": self.timestamp(),
                 "limit": limit,
                 "startTime": startTime,
                 "endTime": endTime,
+                "recvWindow": self.recvWindow,
+                "timestamp": self.timestamp(),
             }
         )
 
@@ -604,7 +786,12 @@ class Client:
         """
         req = "income?"
         querystring = urllib.parse.urlencode(
-            {"symbol": self.symbol, "timestamp": self.timestamp(), "limit": limit}
+            {
+                "symbol": self.symbol,
+                "recvWindow": self.recvWindow,
+                "limit": limit,
+                "timestamp": self.timestamp(),
+            }
         )
 
         return self._get_request(req, querystring)
@@ -614,7 +801,9 @@ class Client:
         POST
         """
         req = "listenKey?"
-        querystring = urllib.parse.urlencode({"timestamp": self.timestamp()})
+        querystring = urllib.parse.urlencode(
+            {"recvWindow": self.recvWindow, "timestamp": self.timestamp()}
+        )
 
         return self._post_request(req, querystring)
 
@@ -626,7 +815,9 @@ class Client:
         PUT
         """
         req = "listenKey?"
-        querystring = urllib.parse.urlencode({"timestamp": self.timestamp()})
+        querystring = urllib.parse.urlencode(
+            {"recvWindow": self.recvWindow, "timestamp": self.timestamp()}
+        )
 
         return self._put_request(req, querystring)
 
@@ -635,18 +826,24 @@ class Client:
         DELETE
         """
         req = "listenKey?"
-        querystring = urllib.parse.urlencode({"timestamp": self.timestamp()})
+        querystring = urllib.parse.urlencode(
+            {"recvWindow": self.recvWindow, "timestamp": self.timestamp()}
+        )
 
         return self._delete_request(req, querystring)
 
-    def user_update_socket(self, on_message, on_error, on_close):
+    def user_update_socket(
+        self,
+        on_message=lambda ws, message: (
+            stdout.write(f"\r{json.loads(message)}"),
+            print(),
+        ),
+        on_error=lambda ws, error: print(error),
+        on_close=lambda ws: print("### closed ###"),
+    ):
 
         listen_key = self.get_listen_key()
         self.open_socket(f"{self.wss_way}{listen_key}", on_message, on_error, on_close)
 
     def stop_user_update_socket(self):
         self.close_stream()
-
-
-def on_new_candle_loaded(ws, candle):
-    print(ws, candle)
